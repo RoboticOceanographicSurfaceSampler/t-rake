ab077932acd08b36a876a5e5ab6d554824916a4c
Louis Ross <louis.ross@gmail.com>
2024-05-16 13:15:09 -0700

    Continuing installer

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-apt install python3-watchdog
+apt install -y python3-watchdog
+mkdir /trake
+mkdir /trake/configuration
+mkdir /trake/data
+cp ./start-trake.sh /usr/local/bin
+chmod 744 /usr/local/bin/start-trake.sh
+(crontab -l ; echo "@reboot /usr/local/bin/start-trake.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
