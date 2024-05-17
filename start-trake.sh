6f8e8c890683773e5a8b98bad32e4a10371cc75d
Louis Ross <louis.ross@gmail.com>
2024-05-17 15:32:44 -0700

    Corrected bug in startup script

--- start-trake.sh
+++ start-trake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Deploying Temperature Rake data acquisition" >> /home/trake/trake.log
-python3 /home/trake/github/src/deploy.py
+python3 /home/trake/github/t-rake/src/deploy.py
 echo "Temperature Rake data acquisition complete" >> /home/trake/trake.log
 echo "Stopping system" >> /home/trake/trake.log
 shutdown -h now
