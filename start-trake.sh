5565dfbbe67b5c0be301cf713034bf35cb13df91
Louis Ross <louis.ross@gmail.com>
2024-05-17 15:27:12 -0700

    Auto start t-rake on boot

--- start-trake.sh
+++ start-trake.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/home/trake/github/src/deploy.py
+echo "Deploying Temperature Rake data acquisition" >> /home/trake/trake.log
+python3 /home/trake/github/src/deploy.py
+echo "Temperature Rake data acquisition complete" >> /home/trake/trake.log
+echo "Stopping system" >> /home/trake/trake.log
+shutdown -h now
