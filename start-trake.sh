614159ac39461d36634ad141986faa7a3aca987e
Louis Ross <louis.ross@gmail.com>
2024-05-17 15:54:28 -0700

    Working deployment script

--- start-trake.sh
+++ start-trake.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd /home/trake/github/t-rake/src
 echo "Deploying Temperature Rake data acquisition" >> /home/trake/trake.log
-python3 /home/trake/github/t-rake/src/deploy.py
+python3 deploy.py
 echo "Temperature Rake data acquisition complete" >> /home/trake/trake.log
 echo "Stopping system" >> /home/trake/trake.log
 shutdown -h now
