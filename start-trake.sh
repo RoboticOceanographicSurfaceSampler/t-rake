85c355e8a1b0216daed28f0a5e258f7a065a7dbd
Louis Ross <louis.ross@gmail.com>
2024-05-17 16:14:48 -0700

    Power off correctly after complete

--- start-trake.sh
+++ start-trake.sh
@@ -5,4 +5,4 @@ echo "Deploying Temperature Rake data acquisition" >> /home/trake/trake.log
 python3 deploy.py
 echo "Temperature Rake data acquisition complete" >> /home/trake/trake.log
 echo "Stopping system" >> /home/trake/trake.log
-shutdown -h now
+/usr/sbin/poweroff
