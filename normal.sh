f17e9eea120588be4eba6b33e7d633e9b6039b1c
Louis Ross <louis.ross@gmail.com>
2024-05-18 12:26:18 -0700

    Fixed privilege bug in utility scripts

--- normal.sh
+++ normal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo cp start-trake.sh /usr/local/bin/start-trake.sh
-echo "Changed startup script to start-trake.sh" >> /home/trake/trake.log
+sudo echo "Changed startup script to start-trake.sh" >> /home/trake/trake.log
 echo "Reboot to restart normal trake acquisition"
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo cp test-trake.sh /usr/local/bin/start-trake.sh
-echo "Changed startup script to test-trake.sh" >> /home/trake/trake.log
+sudo echo "Changed startup script to test-trake.sh" >> /home/trake/trake.log
 echo "Reboot to invoke test-mode trake acquisition"
