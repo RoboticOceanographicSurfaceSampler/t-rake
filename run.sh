d56a55b1df5f24e593fd41f2b557fca40231d590
Louis Ross <louis.ross@gmail.com>
2024-05-19 13:19:22 -0700

    Use driver diagnostic

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 cd /home/trake/github/t-rake/src
 echo "Test Deploying Temperature Rake data acquisition" >> /home/trake/trake.log
-python3 deploy.py debug
+python3 deploy.py driver
 echo "Test Temperature Rake data acquisition complete" >> /home/trake/trake.log
