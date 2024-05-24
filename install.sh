bf0166aaea1a38134738be446a2e01d015a32aff
Louis Ross <louis.ross@gmail.com>
2024-05-24 10:17:18 -0700

    Updated install, added RTC set

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt install -y python3-smbus
 apt install -y python3-watchdog
 mkdir /trake
 mkdir /trake/configuration
@@ -7,6 +8,7 @@ mkdir /trake/data
 cp ./start-trake.sh /usr/local/bin
 chmod 744 /usr/local/bin/start-trake.sh
 (crontab -l ; echo "@reboot /usr/local/bin/start-trake.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+python3 ./set_rtc_datetime.py >> /home/trake/trake.log
 cd src
 gcc -Wall -pthread -fpic -shared -o ad7616_driver.so ad7616_driver.c -lpigpio -lrt
 cd ..
--- start-trake.sh
+++ start-trake.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /home/trake/github/t-rake/src
+python3 ./set_time_from_rtc.py >> /home/trake/trake.log
 echo "Deploying Temperature Rake data acquisition" >> /home/trake/trake.log
 python3 deploy.py
 echo "Temperature Rake data acquisition complete" >> /home/trake/trake.log
