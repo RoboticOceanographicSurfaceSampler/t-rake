0afcf0a8f23e127e0e5f57f73a468d158fa38ec4
Louis Ross <louis.ross@gmail.com>
2024-05-24 10:18:46 -0700

    Corrected bug in install

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ mkdir /trake/data
 cp ./start-trake.sh /usr/local/bin
 chmod 744 /usr/local/bin/start-trake.sh
 (crontab -l ; echo "@reboot /usr/local/bin/start-trake.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-python3 ./set_rtc_datetime.py >> /home/trake/trake.log
 cd src
+python3 ./set_rtc_datetime.py >> /home/trake/trake.log
 gcc -Wall -pthread -fpic -shared -o ad7616_driver.so ad7616_driver.c -lpigpio -lrt
 cd ..
 
