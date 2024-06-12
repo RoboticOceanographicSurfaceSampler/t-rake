48339bfea24f6d016700edfb208ffa53f006f288
Louis Ross <louis.ross@gmail.com>
2024-06-12 12:15:40 -0800

    Update installer for new autostart process

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ mkdir /trake/data
 cp ./start-trake.sh /usr/local/bin
 chmod 744 /usr/local/bin/start-trake.sh
 (crontab -l ; echo "@reboot /usr/local/bin/start-trake.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+cp ./start-trake-onboot.sh /usr/local/bin
+chmod 744 /usr/local/bin/start-trake-onboot.sh
+(crontab -l ; echo "@reboot /usr/local/bin/start-trake-onboot.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 cd src
 python3 ./set_rtc_datetime.py >> /home/trake/trake.log
 gcc -Wall -pthread -fpic -shared -o ad7616_driver.so ad7616_driver.c -lpigpio -lrt
